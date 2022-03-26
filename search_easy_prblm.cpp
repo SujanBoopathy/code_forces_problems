f340ddee967f38ea6a63218936ba6a135c829b36
SujanBoopathy <sujansaradha@gmail.com>
2022-03-26 21:48:30 +0530

    problem-solving

---
+++ search_easy_prblm.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,i;
+    cin>>n;
+
+    while(n>0){
+        cin>>i;
+        if(i==1){
+            cout<<"HARD"<<endl;
+            return 0;
+        }
+        n--;
+    }
+    cout<<"EASY"<<endl;
+    return 0;
+}
