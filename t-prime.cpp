9d68549c2bb11a3245db526cea3ae92e3af62dc0
SujanBoopathy <sujansaradha@gmail.com>
2022-04-11 19:51:00 +0530

    problem-solving

--- t-prime.cpp
+++ t-prime.cpp
@@ -5,11 +5,13 @@ int main(){
     cin>>n;
     for(int i=0;i<n;i++){
         cin>>num;
-        int c=0;
-        for(int i=1;i<=num;i++){
+        int c=2;
+        for(int i=2;i<num;i++){
             if(num%i==0){
                 c++;
             }
+            if(c>3)
+                break;
         }
         if(c==3){
             cout<<"YES"<<endl;
