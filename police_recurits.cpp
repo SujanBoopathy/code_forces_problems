62efc45eba2c34ecb2870b1998727f6195db7d5e
SujanBoopathy <sujansaradha@gmail.com>
2022-04-14 22:06:57 +0530

    problem-solving

---
+++ police_recurits.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,police=0,unsolved=0,crime=0;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int temp;
+        cin>>temp;
+        if(temp==1){
+            police++;
+        }
+        else{
+            if(police>0){
+                police+=temp;
+            }
+            else{
+                unsolved++;
+            }
+        }
+    }
+    cout<<unsolved;
+    return 0;
+}
