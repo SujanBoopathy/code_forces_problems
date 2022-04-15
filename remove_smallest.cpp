de18511ca28a2c9362fa5fa33ca81b912a76d97b
SujanBoopathy <sujansaradha@gmail.com>
2022-04-15 21:08:18 +0530

    problem-solving

--- remove_smallest.cpp
+++ remove_smallest.cpp
@@ -1,5 +1,28 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int size;
+        cin>>size;
+        int a[size];
+        for(int j=0;j<size;j++){
+            cin>>a[j];
+        }
+        int index=0,f=0;
+        for(int k=index+1;k<size;k++){
+            if(a[k]-a[index]<=1){
+                continue;
+            }
+            else{
+                f=1;
+            }
+        }
+        if(f==0)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
     return 0;
 }
