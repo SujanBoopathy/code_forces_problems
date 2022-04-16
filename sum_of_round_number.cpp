9d0b5cfde9945f859dd41fee1e0b66cc631b204c
SujanBoopathy <sujansaradha@gmail.com>
2022-04-16 16:04:05 +0530

    problem-solving

--- sum_of_round_number.cpp
+++ sum_of_round_number.cpp
@@ -4,9 +4,22 @@ int main(){
     int n;
     cin>>n;
     for(int i=0;i<n;i++){
-        int num,count=0;
+        int num,count=0,k=0;
         cin>>num;
-        
+        int a[10000];
+        for(int j=1;num%10!=0;j=j*10){
+            a[k]=num%10*j;
+            if(num%10==0)
+                break;
+            num/=10;
+            k++;
+        }
+        a[k]=num;
+        cout<<k+1<<endl;
+        for(int in=0;in<=k;in++){
+            cout<<a[in]<<" ";
+        }
+        cout<<"\n\n----------------"<<endl;
     }
     return 0;
 }
