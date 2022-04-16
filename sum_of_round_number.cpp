072b61dd76505d179466c0e763ea55f1a6cca548
SujanBoopathy <sujansaradha@gmail.com>
2022-04-16 15:31:41 +0530

    problem-solving

---
+++ sum_of_round_number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int num,count=0;
+        cin>>num;
+        
+    }
+    return 0;
+}
