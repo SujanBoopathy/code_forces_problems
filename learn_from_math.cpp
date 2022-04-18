d032d788de167532e4d04c5b4c21adb91b34f249
SujanBoopathy <sujansaradha@gmail.com>
2022-04-18 15:55:43 +0530

    problem-solving

--- learn_from_math.cpp
+++ learn_from_math.cpp
@@ -2,13 +2,6 @@
 using namespace std;
 
 int main(){
-    int n,a,b;
-    cin>>n;
-    a=2,b=n-2;
-    while(isPrime(a) || isPrime(b)){
-        a++;
-        b--;
-    }
-    cout<<a<<" "<<b;
+    
     return 0;
 }
