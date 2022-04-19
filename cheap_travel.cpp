e0bea64b1ce8cafb8c7a31c18f9076f9869a331d
SujanBoopathy <sujansaradha@gmail.com>
2022-04-19 21:30:59 +0530

    problem_solving

--- cheap_travel.cpp
+++ cheap_travel.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
 int main(){
     int n,m,a,b,cost=0; 
     cin>>n>>m>>a>>b; 
     if(m*a>=b){
-        cost=(n/m)*b+min((n%m)*a,b);
+        cost=(n/m)
     }
     else{
         cost=n*a;
