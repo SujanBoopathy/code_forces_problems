f865cf39a12cc5213821c696472920c21f9c2df7
SujanBoopathy <sujansaradha@gmail.com>
2022-04-29 19:32:07 +0530

    problem-solving

--- raising_bacteria.cpp
+++ raising_bacteria.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-    if(n%2==0)
-        cout<<1<<endl;
-    else
-        cout<<2<<endl;
+    long long int x,count=0;
+    cin>>x;
+    while(x/2!=0){
+        if(x%2==1)
+            count++;
+        x/=2;
+    }
+    cout<<count+1<<endl;
     return 0;
 }
