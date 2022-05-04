d32938998cf96ade9b1d1de977406d349c65d40f
SujanBoopathy <sujansaradha@gmail.com>
2022-05-04 18:42:48 +0530

    problem-solving

--- boring_apartment.cpp
+++ boring_apartment.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int x,d=0;
+        cin>>x;
+        int a=x%10;
+        string s=to_string(x);
+        for(int i=0;i<s.size();i++){
+            d+=i+1;
+        }
+        cout<<((a-1)*10)+d<<endl;
+    }
     return 0;
 }
+
