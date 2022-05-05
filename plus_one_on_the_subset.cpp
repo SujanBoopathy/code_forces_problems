40616eff6bc021c1ce165c4231e36afed45b5308
SujanBoopathy <sujansaradha@gmail.com>
2022-05-05 18:44:03 +0530

    problem_solving

--- plus_one_on_the_subset.cpp
+++ plus_one_on_the_subset.cpp
@@ -1,5 +1,21 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int size,maxi=0,count=0;
+        cin>>size;
+        int a[size];
+        for(int i=0;i<size;i++){
+            cin>>a[i];
+            int temp=a[i];
+            maxi=max(temp,maxi);
+        }
+        for(int i=0;i<size;i++){
+            count+=maxi-a[i];
+        }
+        cout<<count<<endl;
+    }
     return 0;
 }
