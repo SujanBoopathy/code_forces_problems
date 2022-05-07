d474db1abdbb9ad976f20c8c0091e138c3f53a2b
SujanBoopathy <sujansaradha@gmail.com>
2022-05-07 19:28:00 +0530

    problem_solving

--- sereja_and_dima.cpp
+++ sereja_and_dima.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int main(){
-    int n,sum1=0,sum2=0,temp;
+    int n,sum1=0,sum2=0;
     cin>>n;
-    int mid=n/2;
     int a[n];
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    for(int i=0;i<mid;i++){
+    sort(a,a+n);
+    for(int i=n-1;i>=0;i-=2){
         sum1+=a[i];
     }
-    for(int i=n-1;i>=mid;i--){
+    for(int i=n-2;i>=0;i-=2){
         sum2+=a[i];
     }
-    cout<<sum2<<" "<<sum1<<endl;
-    return 0;
+    cout<<sum1<<" "<<sum2<<endl;
 }
