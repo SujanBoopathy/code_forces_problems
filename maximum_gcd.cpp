a78ea69e9e808e557ece4ce7f60c1312a79910c5
SujanBoopathy <sujansaradha@gmail.com>
2022-05-08 21:42:03 +0530

    problem_solving

--- maximum_gcd.cpp
+++ maximum_gcd.cpp
@@ -1,5 +1,25 @@
 #include<iostream>
 using namespace std;
+int gcd(int a,int b){
+    if(a==0) return b;
+    if(b==0) return a;
+    if(a==b) return a;
+    if(a>b) return gcd(a-b,b);
+    return gcd(a,b-a);
+
+}
 int main(){
+    int n,m=0;
+    cin>>n;
+    while(n--){
+        int s;
+        cin>>s;
+        for(int i=1;i<=s;i++){
+            for(int j=i+1;j<=s;j++){
+                m=max(m,gcd(i,j));
+            }
+        }
+        cout<<m<<endl;
+    }
     return 0;
 }
