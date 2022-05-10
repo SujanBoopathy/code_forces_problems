a7f314284e626e802610720f0b002f1d40dd95a9
SujanBoopathy <sujansaradha@gmail.com>
2022-05-10 18:58:02 +0530

    problem-solving

--- fair_division.cpp
+++ fair_division.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int n;
+    cin>>n;
+    while(n--){
+        int testcase,temp,c1=0,c2=0;
+        cin>>testcase;
+        for(int i=0;i<testcase;i++){
+            cin>>temp;
+            if(temp==1)
+                c1++;
+            else 
+                c2++;
+        }
+        if(c1%2==0 && c2%2==0)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
     return 0;
 }
