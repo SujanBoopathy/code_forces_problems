ea1413ca23561a6d4bdffa291daa8c07fc7bd4bf
SujanBoopathy <sujansaradha@gmail.com>
2022-05-12 19:24:13 +0530

    problem_solving

---
+++ minimum_index_sum_of_two_list.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        int m=INT_MAX;
+        set<string> v;
+        vector<string> vect;
+        for(int i=0;i<list1.size();i++){
+            for(int j=0;j<list2.size();j++){
+                if(list1[i]==list2[j]){
+                    if(i+j<m){
+                        m=i+j;
+                        v.insert(list1[i]);
+                    }
+                    if(i+j==m){
+                        v.insert(list1[i]);
+                    }
+                }
+                
+            }
+        }
+        for(auto& a:v){
+            vect.push_back(a);
+        }
+        return vect;
+    }
+};
