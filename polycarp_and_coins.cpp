8979a0a8856786a00d87403da169ef3a6c222573
SujanBoopathy <sujansaradha@gmail.com>
2022-05-13 22:15:31 +0530

    problem_solving

--- polycarp_and_coins.cpp
+++ polycarp_and_coins.cpp
@@ -7,16 +7,10 @@ int main(){
         int total,a,b;
         cin>>total;
         a=total/3;
-        b=a*2;
-        if(a+b==total)
-            cout<<a<<b/2<<endl;
-        else{
-            while(a+b != total && a-b >=2)
-                a+=2;
-            while(a+b!= total )
-                a++;
-            cout<<a<<b/2<<endl;
-        }
+        b=total/3;
+        if(total%3==1)  a++;
+        if(total%3==2)  b++;
+        cout<<a<<" "<<b<<endl;
     }
     return 0;
 }
