d1907e33babae2f92b6901a966ed9960ebc02d95
SujanBoopathy <sujansaradha@gmail.com>
2022-05-15 13:04:12 +0530

    problem_solving

---
+++ again_twenty_five.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n>=2){
+        cout<<25<<endl;
+    }
+    else if(n==1){
+        cout<<5<<endl;
+    }
+    else{
+        cout<<1<<endl;
+    }
+    return 0;
+}
