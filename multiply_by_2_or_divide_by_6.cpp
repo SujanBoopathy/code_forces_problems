24d5f7accf64903fe4d7dd21112e978d2d12d480
SujanBoopathy <sujansaradha@gmail.com>
2022-05-20 20:54:05 +0530

    problem_solving

---
+++ multiply_by_2_or_divide_by_6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int testcase;
+    cin>>testcase;
+    while(testcase--){
+        int num;
+        cin>>num;
+        if()
+    }
+    return 0;
+}
