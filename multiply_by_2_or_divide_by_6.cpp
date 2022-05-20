ea7269f5ecfb78c36a5c6c0997052f25ffcfcb9b
SujanBoopathy <sujansaradha@gmail.com>
2022-05-20 20:57:45 +0530

    problem_solving

--- multiply_by_2_or_divide_by_6.cpp
+++ multiply_by_2_or_divide_by_6.cpp
@@ -4,14 +4,21 @@ int main(){
     int testcase;
     cin>>testcase;
     while(testcase--){
-        int num;
+        int num,count=0;
         cin>>num;
-        if(num%6==0){
+        while(num>1){
+            if(num%6==0){
             num/=6;
+            }
+            else{
+                num*=2;
+            }
+            count++;
         }
-        else{
-            num*=2;
-        }
+        if(num==1)
+            cout<<count<<endl;
+        else
+            cout<<-1<<endl;
     }
     return 0;
 }
