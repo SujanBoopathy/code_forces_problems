ab9365be3c0cea0fe955975296156407c27b8fbe
SujanBoopathy <sujansaradha@gmail.com>
2022-05-21 20:38:44 +0530

    problem_solving

--- brains_photo.cpp
+++ brains_photo.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int row,col,flag=0;
+    cin>>row>>col;
+    char color;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cin>>color;
+            if(color=='C' || color=='M' || color=='Y'){
+                flag=1;
+            }
+        }
+    }
+    if(flag==1){
+        cout<<"#Color"<<endl;
+    }
+    else{
+        cout<<"#Black&White"<<endl;
+    }
     return 0;
 }
