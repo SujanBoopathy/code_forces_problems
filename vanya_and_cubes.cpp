a26aad2a74b62b46579b7ecfd4f2408c00baa036
SujanBoopathy <sujansaradha@gmail.com>
2022-05-22 23:12:05 +0530

    problem_solving

---
+++ vanya_and_cubes.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int reduce(int n){
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        sum+=i;
+    }
+    return sum;
+}
+int main(){
+    int n;
+    cin>>n;
+    int fact=1,count=0;
+    while(n>0){
+        if((n-reduce(fact))>=0)
+        {
+            n=n-reduce(fact);
+            count++;
+        }
+        else{
+            break;
+        }
+        fact++;
+        
+    }
+    cout<<count<<endl;
+    return 0;
+}
