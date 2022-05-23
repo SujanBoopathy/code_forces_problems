55cebe5a3222bd6175314b876f34158000988438
SujanBoopathy <sujansaradha@gmail.com>
2022-05-23 23:36:03 +0530

    problem_solving

--- vasya_and_socks.cpp
+++ vasya_and_socks.cpp
@@ -4,7 +4,14 @@ int main(){
     int n,m,a,b;
     cin>>n>>m;
     int count=n;
-
+    while(1){
+        a=n/m;
+        b=a+(n%m);
+        n=b;
+        count+=a;
+        if(b<m)
+            break;
+    }
     cout<<count<<endl;
     return 0;
 }
