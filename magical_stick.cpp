c50b3576585abde442cbb34645645537b36f61a0
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-07-05 22:09:51 +0530

    Update magical_stick.cpp

--- magical_stick.cpp
+++ magical_stick.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 void main(){
-    int n;
-    cin>>n;
-    while(n--){
+    int trails;
+    cin>>trails;
+    while(trails--){
         int num;
         cin>>num;
         cout<<num/2+num%2<<endl;
