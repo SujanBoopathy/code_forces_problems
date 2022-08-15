b8f70d3c0b59883d7d76ae07780a223e1dea5992
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-08-15 21:32:25 +0530

    Update dislike_of_threes.cpp

--- dislike_of_threes.cpp
+++ dislike_of_threes.cpp
@@ -1,22 +1,22 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main(){
+void main(){
     int n;
     cin>>n;
     while(n--){
         int a,i=1;
         cin>>a;
-        vector<int> v;
+        vector<int> vect;
         while(v.size()!=a){
             if(i%3!=0 && i%10!=3){
-                v.push_back(i);
+                vect.push_back(i);
             }
             i++;   
         }
-        cout<<v[a-1]<<endl;
+        cout<<vect[a-1]<<endl;
     }
-    return 0;
+   
 }
 
 
