1e12dc0be62b11f32a4c309edde2e8757a265a87
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-09-14 21:58:17 +0530

    Update football.cpp

--- football.cpp
+++ football.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main(){
     int count=1;
-    string s;
+    string str;
     cin>>s;
-    for(int i=1;i<=s.length();i++){
-        if(s[i]==s[i-1] ){ count++;
+    for(int i=1;i<=str.length();i++){
+        if(str[i]==str[i-1] ){ count++;
         if(count==7){
             cout<<"YES"<<endl;
             return 0;
