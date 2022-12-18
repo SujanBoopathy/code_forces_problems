d48e3ef7691230e5968c54a61f37af7ec3e53e7c
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-12-18 20:22:31 +0530

    Update beautiful_matrix.cpp

--- beautiful_matrix.cpp
+++ beautiful_matrix.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main(){
     int x;
     int temp[2];
-    for(int i=1;i<=5;i++){
-        for(int j=1;j<=5;j++){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
             cin>>x;
             if(x==1){
                 temp[0]=i;
