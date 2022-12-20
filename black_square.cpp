8655954ed319f554f76ac6864f63285effb341c8
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-12-20 21:30:55 +0530

    problem-solving

--- black_square.cpp
+++ black_square.cpp
@@ -3,16 +3,16 @@ using namespace std;
 int main(){
     int a,b,c,d,ans=0;
     cin>>a>>b>>c>>d;
-    string s;
-    cin>>s;
-    for(int i=0;i<s.length();i++){
-        if(s[i]=='1')
+    string str;
+    cin>>str;
+    for(int i=0;i<str.length();i++){
+        if(str[i]=='1')
             ans+=a;
-        else if(s[i]=='2')
+        else if(str[i]=='2')
             ans+=b;
-        else if(s[i]=='3')
+        else if(str[i]=='3')
             ans+=c;
-        else if(s[i]=='4')
+        else if(str[i]=='4')
             ans+=d;
     }
     cout<<ans<<endl;
